46a6d70a2597545acb1ad69015976453bbd916c4
Egor Muzykantov <linemail@mail.ru>
2023-04-17 01:12:01 +0300

    Wrote the code to solve the task

--- Final_HW/Program.cs
+++ Final_HW/Program.cs
@@ -1,1 +1,51 @@
 ﻿
+Console.Clear();
+
+string[] stringArray;
+string[] correctArray;
+// arrayString = new string[] {"hello", "2", "world", ":-)"};
+
+Console.WriteLine("Введите элементы массива через пробел: ");
+
+stringArray = InputElements(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries));
+Console.WriteLine($"[{string.Join(", ", stringArray)}]");
+
+correctArray = FillArrary(stringArray);
+Console.WriteLine($"[{string.Join(", ", correctArray)}]");
+
+string[] InputElements(string[] array)
+{
+    if (IsEmptyArray(array)) return array;
+    else
+    {
+        Console.WriteLine("Вы ничего не ввели, пожалуйста повторите: ");
+        return InputElements(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries));
+    }
+}
+
+bool IsEmptyArray(string[] arr)
+{
+    if (arr == null || arr.Length == 0) return false;
+    return true;
+}
+
+string[] FillArrary(string[] array)
+{
+    int count = 0;
+
+    foreach (string element in array)
+    {
+        if (element.Length <= 3) count++;
+    }
+
+    string[] result = new string[count];
+
+    for (int i = 0, j = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[j] = array[i]; j++;
+        }
+    }
+    return result;
+}
